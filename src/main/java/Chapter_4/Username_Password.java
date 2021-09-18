a5f334e2ee322cdc7aa6d65dccf73b01e64205cb
doquocang <doquocan64@gmail.com>
2021-09-18 20:27:55 +0700

    Learn

---
+++ src/main/java/Chapter_4/Username_Password.java
@@ -0,0 +1,26 @@
+package Chapter_4;
+
+import java.util.Scanner;
+/**
+ *
+ * @author admin
+ */
+public class Username_Password {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String input_u, input_p, u, p;
+        u = "HuyHoang";
+        p = "2021";
+        
+        System.out.println("Enter Username: ");
+        input_u = input.nextLine();
+        System.out.println("Enter Password: ");
+        input_p = input.nextLine();
+        
+        if ((input_u.equals(u)) && input_p.equals(p)) {
+            System.out.println("Correct account and password!");
+        } else {
+            System.out.println("Incorrect account or password!");
+        }
+    }
+}
